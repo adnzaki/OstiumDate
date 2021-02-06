f183b9f1108c5ae95aa8953f7e149bbc44a0eb98
adnzaki <adnan_zaki@hotmail.co.id>
2021-02-06 16:05:07 +0700

    - Menambahkan tipe output "num-only" untuk menampilkan jumlah hari saja - Menyederhanakan kode pada Calculation.php

--- OstiumDate/Calculation.php
+++ OstiumDate/Calculation.php
@@ -12,6 +12,7 @@
  * @link		https://wolestech.com
  */
 
+
 require_once 'helper.php';
 
 class Calculation 
@@ -20,68 +21,38 @@ class Calculation
      * Fungsi untuk menambah jumlah hari pada tanggal 
      * 
      * @param string $date now | dd-mm-yyyy
-     * @param mixed $nums, example: array -> ['d', 'm', 'y'] | int -> 0
+     * @param string|array $nums
+     * 
+     * @return string
      */
     public function add(string $date = 'now', $nums = ''): string
     {
-        if(empty($nums))
-        {
-            return $this->error('empty');
-        }
-        else 
-        {
-            if($date === 'now')
-            {
-                $date = date('Y-m-d');
-            }
-            else 
-            {
-                $date = reverse($date, '-', '-');
-            }
-
-            // cek tipe data $nums untuk membuat interval
-            if(gettype($nums) === 'integer')
-            {
-                $interval = new DateInterval('P'.(string)$nums.'D');
-            }
-            elseif(gettype($nums) === 'array')
-            {
-                if(! array_key_exists('d', $nums))
-                {
-                    $nums['d'] = 0;
-                }
-
-                if(! array_key_exists('m', $nums))
-                {
-                    $nums['m'] = 0;
-                }
-
-                if(! array_key_exists('y', $nums))
-                {
-                    $nums['y'] = 0;
-                }
-
-                $days = (string)$nums['d'];
-                $months = (string)$nums['m'];
-                $year = (string)$nums['y'];
-
-                // 'P[year]Y[months]M[days]D'
-                $interval = new DateInterval('P'.$year.'Y'.$months.'M'.$days.'D');
-            }
-
-            $dt = new DateTime($date);
-            $dt->add($interval);
-            return $dt->format('d-m-Y');
-        }
+        return $this->calculate('add', $date, $nums);
     }    
 
     /**
      * Fungsi untuk mengurangi jumlah hari pada tanggal 
      * 
      * @param string $date now | dd-mm-yyyy
-     * @param mixed $nums, example: array -> ['d', 'm', 'y'] | int -> 0
+     * @param string|array $nums
+     * 
+     * @return string
      */
     public function sub(string $date = 'now', $nums = ''): string
+    {
+        return $this->calculate('sub', $date, $nums);
+    } 
+
+    /**
+     * Pemroses perhitungan tambah dan kurangi tanggal
+     * 
+     * @param string $type
+     * @param string $date
+     * @param string|array $nums
+     * 
+     * @return string
+     */
+    private function calculate(string $type, string $date = 'now', $nums = ''): string
     {
         if(empty($nums))
         {
@@ -128,19 +99,29 @@ class Calculation
                 $interval = new DateInterval('P'.$year.'Y'.$months.'M'.$days.'D');
             }
 
-            $dt = new DateTime($date);
-            $dt->sub($interval);
-            return $dt->format('d-m-Y');
+            $dt = new DateTime($date);   
+            if($type === 'add')
+            {
+                $dt->add($interval);
+            }
+            else
+            {
+                $dt->sub($interval);
+            }
+
+            return $dt->format('d-m-Y');         
         }
-    } 
+    }
 
     /**
      * Fungsi untuk menghitung selisih hari/bulan/tahun
      * 
      * @param string $date1 
      * @param string $date2 
-     * @param string $printIn Opsi tersedia: [pn-days, total-days, month, year, y-m-d, m-d, y-d, y-m]
+     * @param string $printIn Opsi tersedia: [pn-days, total-days, num-only, month, year, y-m-d, m-d, y-d, y-m]
      * @param string $countFrom Opsi tersedia: [a-b, b-a], hanya untuk $printIn = 'pn-days
+     * 
+     * @return string
      */
     public function diff(string $date1, string $date2, string $printIn, $countFrom = 'a-b'): string 
     {
@@ -153,6 +134,7 @@ class Calculation
         switch ($printIn) {
             case 'pn-days': $outputText = '%R%a hari'; break;
             case 'total-days': $outputText = '%a hari'; break;
+            case 'num-only': $outputText = '%a'; break;
             case 'month': $outputText = '%m bulan'; break;
             case 'year': $outputText = '%y tahun'; break;
             case 'y-m-d': $outputText = '%y tahun, %m bulan, %d hari'; break;
